0f7088497557895011dd1f624e29ec2f9ba8eb41
Roger Wells <rsw359@gmail.com>
2022-02-09 16:42:53 +0900

    edited scripts and added the commented out console logs

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ let userName = prompt('What is your name?')
 alert(`Welcome, ${userName} This is the place to be`)
 
 //Quest. 1//
-  let answerOne = prompt('Do I have a BMC').toLowerCase();
+  let answerOne = prompt('Do I have a BMC road bike?').toLowerCase();
 
   if (answerOne === 'yes' || answerOne === 'y') {
     alert(`That\'s correct, ${userName}. I do.`);
@@ -22,7 +22,7 @@ alert(`Welcome, ${userName} This is the place to be`)
     alert(`That\'s correct, ${userName}. I do.`);
 
   } else {
-    alert(`No, that\'s not correct, ${userName}. I live in Japan.`);
+    alert(`No, I got you on that one, ${userName}. I live in Japan.`);
   }
 
 
@@ -32,17 +32,17 @@ alert(`Welcome, ${userName} This is the place to be`)
     alert(`That\'s right, ${userName}. I was born in San Diego.`); 
 
   } else if (answerThree === 'yes' || answerThree=== 'y') {
-    alert(`No, that\'s not correct, ${userName}. I was born in San Diego.`);
+    alert(`No, that\'s not quite right, ${userName}. I was born in San Diego.`);
   }
   
 //Quest. 4//
-let answerFour = prompt('Am I an English teacher?').toLowerCase();
+let answerFour = prompt('Was I born in 1982?').toLowerCase();
 
 if (answerFour === 'yes' || answerFour === 'y') {
-  alert(`That\'s correct, ${userName}. I am.`);
+  alert(`That\'s correct, ${userName}. I was.`);
 
 } else if (answerFour === 'no' || answerFour === 'n') {
-  alert(`No, that\'s not correct, ${userName}. I AM a teacher.`);
+  alert(`No, ${userName}. I actually was born in '82.`);
 }
 
 
@@ -56,8 +56,39 @@ if (answerFive === 'yes' || answerFive === 'y') {
   alert(`No, that\'s not correct, ${userName}. I do enjoy photography.`);
 }
 
-console.log('User entered' + ' ' + answerOne)
-console.log('User entered' + ' ' + answerTwo)
-console.log('User entered' + ' ' + answerThree)
-console.log('User entered' + ' ' + answerFour)
-console.log('User entered' + ' ' + answerFive)
+alert(`Thanks for the answers, ${userName}. Let's get to know each other a bit.`);
+
+
+// if (answerOne === 'yes' || answerOne === 'y') {
+//   console.log(`You got \#1 ${userName}`)
+// }
+
+// if (answerTwo === 'japan'){
+//   console.log(`Keep it up\, ${userName}! \#2 was right`)
+// }
+
+// if (answerThree === 'no' || answerThree === 'n') {
+//  console.log(`\#3 was right \' too ${userName}`)
+// }
+
+// if (answerFour === 'yes' || answerFour === 'y') {
+//   console.log(`I bet you feel real smart\, ${userName}. Good, you are, #4 was right!`)
+// }
+
+// if (answerFive === 'yes' || answerFive === 'y') {
+//   console.log(`You got question 5 right\,too\, ${userName}`)
+
+// } else if (answerOne === 'yes' || answerOne === 'y' &&
+//     answerTwo === 'japan' && 
+//     answerThree === 'no' || answerThree === 'n' &&
+//     answerFour === 'yes' || answerFour === 'y' &&
+//     answerFive === 'yes' || answerFive === 'y'){
+
+//       console.log(`YOU GOT THEM ALL RIGHT\, ${userName}. 
+//       Maybe it was worth staying up all night to build this after all!`)
+//   } else {
+//       console.log('After you read my page\, you can try again')
+//   }
+
+
+
